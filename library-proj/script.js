e9b06dd8b73c9cc1f36e5a995c624c3b2e8ec563
Y. Violet Guo <violetguos@users.noreply.github.com>
2020-09-10 13:52:09 -0300

    Add remove book card

--- library-proj/script.js
+++ library-proj/script.js
@@ -10,10 +10,38 @@ function Book(title, read, author, numberOfPages) {
     const contentContainer = document.querySelector('#content');
     const newBook = document.createElement('div');
     newBook.className = 'book';
+    newBook.id = this.title;
+    this.bookCardDiv = newBook;
+
+    const title = document.createElement('h4');
+    title.textContent = this.title;
+
+    const author = document.createElement('p');
+    author.textContent = this.author;
+
+    const numberOfPages = document.createElement('p');
+    numberOfPages.textContent = this.numberOfPages;
+
+    const read = document.createElement('button');
+    read.textContent = this.read ? 'Read' : 'Not read';
+    read.addEventListener('click', () => {
+      this.read = !this.read;
+      read.textContent = this.read ? 'Read' : 'Not read';
+    });
 
     const removeButton = document.createElement('button');
     removeButton.className = 'removeBook';
     removeButton.textContent = 'Remove';
+
+    removeButton.addEventListener('click', () => {
+      const node = document.getElementById(this.title);
+      contentContainer.removeChild(node);
+    });
+
+    newBook.appendChild(title);
+    newBook.appendChild(author);
+    newBook.appendChild(numberOfPages);
+    newBook.appendChild(read);
     newBook.appendChild(removeButton);
 
     contentContainer.appendChild(newBook);
@@ -23,7 +51,7 @@ function Book(title, read, author, numberOfPages) {
 function addBookToLibrary() {
   const book = new Book('Catcher of the rye', true, 'J. D. Salinger', 200);
   myLibrary.push(book);
-  myLibrary[0].display();
+  book.display();
 }
 
 // main
