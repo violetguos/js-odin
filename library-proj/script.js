e290412508f2ee24f2535f7a09bade44dee82a01
Y. Violet Guo <violetguos@users.noreply.github.com>
2020-09-10 12:58:03 -0300

    Add new book with remove button

--- library-proj/script.js
+++ library-proj/script.js
@@ -5,12 +5,27 @@ function Book(title, read, author, numberOfPages) {
   this.read = read;
   this.author = author;
   this.numberOfPages = numberOfPages;
+
+  this.display = function () {
+    const contentContainer = document.querySelector('#content');
+    const newBook = document.createElement('div');
+    newBook.className = 'book';
+
+    const removeButton = document.createElement('button');
+    removeButton.className = 'removeBook';
+    removeButton.textContent = 'Remove';
+    newBook.appendChild(removeButton);
+
+    contentContainer.appendChild(newBook);
+  };
 }
 
 function addBookToLibrary() {
   const book = new Book('Catcher of the rye', true, 'J. D. Salinger', 200);
   myLibrary.push(book);
+  myLibrary[0].display();
 }
 
-addBookToLibrary();
-console.log(myLibrary);
+// main
+const newBookButton = document.querySelector('.newBook');
+newBookButton.addEventListener('click', addBookToLibrary);
